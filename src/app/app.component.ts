75f2f520710c6f058f07b40f61b1c39175bd885c
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-09-12 19:48:28 -0700

    refactored code to include OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  possibleColors = ['red', 'blue', 'yellow', 'black', 'orange', 'pink', 'green', 'brown', 'gray', 'purple', 'pink', 'aqua', 'indigo', 'white', 'tomato'];
-  colors = [
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-    this.possibleColors[Math.floor(Math.random()*this.possibleColors.length)],
-  ];
+export class AppComponent implements OnInit {
+  colors = [];
+
+  makeColorsArray() {
+    var possibleColors = ['red', 'blue', 'yellow', 'black', 'orange', 'pink', 'green', 'brown', 'gray', 'purple', 'pink', 'aqua', 'indigo', 'white', 'tomato'];
+    for (let i = 0; i < 10; i++) {
+      this.colors.push(possibleColors[Math.floor(Math.random()*possibleColors.length)]);
+    }
+  }
+
+  ngOnInit() {
+    this.makeColorsArray();
+  }
 }
